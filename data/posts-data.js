7dcfc564ff6da7a404b741b67d7de49a8810eef7
hangz007 <hangz00749@163.com>
2019-01-11 21:28:59 +0800

    独立数据到单独的数据文件中
    独立数据到单独的数据文件中

---
+++ data/posts-data.js
@@ -0,0 +1,60 @@
+var local_database = [
+  {
+    date: "Sep 18 2016",
+    title: "正是虾肥蟹壮时",
+    imgSrc: "/images/post/crab.png",
+    avatar: "/images/avatar/1.png",
+    content: "虾肥蟹壮正当时,此时不吃待何时",
+    reading: "112",
+    collection: "96"
+  },
+  {
+    date: "Sep 18 2016",
+    title: "比利林恩的故事",
+    imgSrc: "/images/post/bl.png",
+    avatar: "/images/avatar/1.png",
+    content: "比利林恩传奇的中场故事",
+    reading: "92",
+    collection: "80"
+  },
+  {
+    date: "Nov 21 2018",
+    title: "当我们在谈论经济时，在谈论什么？",
+    imgSrc: "/images/post/cat.png",
+    avatar: "/images/avatar/3.png",
+    content: "没有人不关心经济，经济影响到工作的稳定",
+    reading: "133",
+    collection: "60"
+  },
+  {
+    date: "Nov 21 2018",
+    title: "微应用开发",
+    imgSrc: "/images/post/xiaolong.jpg",
+    avatar: "/images/avatar/3.png",
+    content: "微应用主要特点是免安装、体积小、速度快，即用即走",
+    reading: "42",
+    collection: "123"
+  },
+  {
+    date: "Nov 21 2018",
+    title: "这几天,闲来无事，在学习微信小程序开发",
+    imgSrc: "/images/post/sls.jpg",
+    avatar: "/images/avatar/3.png",
+    content: "近几年来，微信小程序逐渐流行开来",
+    reading: "120",
+    collection: "43"
+  },
+  {
+    date: "Nov 21 2018",
+    title: "科学家发现外星生命体啦",
+    imgSrc: "/images/post/vr.png",
+    avatar: "/images/avatar/5.png",
+    content: "对于科学家发现外星生命体这件事，刘慈欣建议不要回答",
+    reading: "150",
+    collection: "75"
+  }
+]
+
+module.exports = {
+  postList:local_database
+}
