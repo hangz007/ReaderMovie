88ea708c61db5baa9eaa61aae64f5f137e30fd78
hangz007 <hangz00749@163.com>
2019-07-07 18:37:42 +0800

    使用缓存实现文章收藏功能
    使用缓存实现文章收藏功能，调用模态框接口实现分享功能

--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -1,65 +1,141 @@
+var postData = require('../../../data/posts-data.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad: function(options) {
+    var postId = options.id;
+    var post = postData.postList[postId];
+    this.data.currentPostId = postId;
+    this.setData({
+      postData: post
+    });
+    var postsCollected = wx.getStorageSync("posts_collected");
+    if (postsCollected) {
+      var postCollected = postsCollected[postId];
+      this.setData({
+        collected: postCollected
+      });
+    } else {
+      var postsCollected = {};
+      postsCollected[postId] = false;
+      wx.setStorageSync("posts_collected", postsCollected);
+    }
   },
-
+  onCollectionTap: function(event) {
+    var postsCollected = wx.getStorageSync("posts_collected");
+    var postId = this.data.currentPostId;
+    var postCollected = postsCollected[postId];
+    // 收藏改为未收藏，未收藏改为收藏
+    postCollected = !postCollected;
+    postsCollected[postId] = postCollected;
+    this.showToast(postsCollected, postCollected);
+  },
+  // 模态框
+  showModal: function (postsCollected,postCollected) {
+    var that = this;
+    wx.showModal({
+      title: '收藏',
+      content: postCollected?"收藏该文章？":"取消收藏该文章？",
+      showCancel:true,
+      cancelText:'取消',
+      cancelColor:'#333',
+      confirmText:'确认',
+      confirmColor:'#405f80',
+      success:function(res) {
+        if(res.confirm) {
+          // 更新当前文章的收藏状态缓存
+          wx.setStorageSync("posts_collected", postsCollected);
+          // 更新数据
+          that.setData({ collected: postCollected });
+        }
+      }
+    })
+  },
+  showToast: function (postsCollected, postCollected) {
+    var that = this;
+    // 更新当前文章的收藏状态缓存
+    wx.setStorageSync("posts_collected", postsCollected);
+    // 更新数据
+    that.setData({ collected: postCollected });
+    wx.showToast({
+      title: postCollected ? "收藏成功" : "取消成功",
+      duration: 1000,
+      icon: "success"
+    })
+  },
+  onShareTap:function(event) {
+    var itemList=[
+      "分享给微信好友",
+      "分享到朋友圈",
+      "分享到QQ",
+      "分享到微博"
+    ]
+    wx.showActionSheet({
+      itemList:itemList,
+      itemColor:"#405f80",
+      success:function(res) {
+        wx.showModal({
+          title: '用户'+itemList[res.tapIndex],
+          content: '现在无法实现分享功能'
+        });
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-    
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-    
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-    
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-    
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-    
+  onPullDownRefresh: function() {
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
-    
+  onReachBottom: function() {
+
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
-    
+  onShareAppMessage: function() {
+
   }
 })
