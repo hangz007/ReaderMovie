4d02c486eb702f98785ed093780bf43b012e5cf8
hangz007 <hangz00749@163.com>
2019-08-14 11:57:40 +0800

    使用请求获取服务器数据
    1、配置全局app.js，设置doubanBase网址
    2、使用api获取服务器正在热映、即将上映、top250的数据。

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -1,4 +1,5 @@
 // pages/movies/movies.js
+var app = getApp();
 Page({
 
   /**
@@ -12,9 +13,35 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 正在热映
+    var webUrl = app.globalData.doubanBase;
+    var inTheatersUrl = webUrl+"/v2/movie/in_theaters";
+    // 即将上映
+    var comingSoonUrl = webUrl+"/v2/movie/coming_soon";
+    // 最热的250部电影
+    var top250Url = webUrl+"/v2/movie/top250";
+    // 异步请求
+    this.getMovieListData(inTheatersUrl);
+    this.getMovieListData(comingSoonUrl);
+    this.getMovieListData(top250Url);
 
   },
 
+  getMovieListData:function(url) {
+    wx.request({
+      url: url,
+      method: 'GET',
+      header:{
+        "Content-Type":"json"
+      },
+      success:function(res) {
+          console.log(res);
+      },
+      fail:function() {
+        
+      }
+    })
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
