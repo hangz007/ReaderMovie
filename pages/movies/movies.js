91f3ccf92a5902d00f774e7c7db6a034b2859ff9
hangz007 <hangz00749@163.com>
2019-08-16 11:59:46 +0800

    bug修复和电影搜索页面功能实现
    1、修复音乐播放完成后图标没有复位的bug
    2、修复movies处理数据时的bug
    3、实现电影搜索页面查询功能

--- pages/movies/movies.js
+++ pages/movies/movies.js
@@ -8,95 +8,101 @@ Page({
    * 页面的初始数据
    */
   data: {
-    inTheaters:{},
-    comingSoon:{},
-    top250:{},
-    containerShow:true,
-    searchPannelShow:false
+    inTheaters: {},
+    comingSoon: {},
+    top250: {},
+    searchResult: {},
+    containerShow: true,
+    searchPannelShow: false
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     // 正在热映
     var webUrl = app.globalData.doubanBase;
-    var inTheatersUrl = webUrl+"/v2/movie/in_theaters"+"?start=0&count=3";
+    var inTheatersUrl = webUrl + "/v2/movie/in_theaters" + "?start=0&count=3";
     // 即将上映
     var comingSoonUrl = webUrl + "/v2/movie/coming_soon" + "?start=0&count=3";
     // 最热的250部电影
     var top250Url = webUrl + "/v2/movie/top250" + "?start=0&count=3";
     // 异步请求
-    this.getMovieListData(inTheatersUrl,"inTheaters","正在热映");
-    this.getMovieListData(comingSoonUrl,"comingSoon","即将上映");
-    this.getMovieListData(top250Url,"top250","豆瓣Top250");
+    this.getMovieListData(inTheatersUrl, "inTheaters", "正在热映");
+    this.getMovieListData(comingSoonUrl, "comingSoon", "即将上映");
+    this.getMovieListData(top250Url, "top250", "豆瓣Top250");
   },
-  onMoreTap:function(event) {
+  onMoreTap: function(event) {
     // 获取电影分类类型
     var category = event.currentTarget.dataset.category;
     wx.navigateTo({
       url: 'more-movie/more-movie?category=' + category
     });
   },
-  getMovieListData: function (url, settedKey,categoryTitle) {
+  getMovieListData: function(url, settedKey, categoryTitle) {
     var that = this;
     wx.request({
       url: url,
       method: 'GET',
-      header:{
-        "Content-Type":"json"
+      header: {
+        "Content-Type": "json"
       },
-      success:function(res) {
+      success: function(res) {
         //  console.log(res);
-        that.processDoubanData(res.data, settedKey,categoryTitle);
+        that.processDoubanData(res.data, settedKey, categoryTitle);
 
       },
-      fail:function() {
-        
+      fail: function() {
+
       }
     })
   },
   // 处理豆瓣数据
-  processDoubanData: function (movicesDouban, settedKey,categoryTitle) {
+  processDoubanData: function(movicesDouban, settedKey, categoryTitle) {
     var movies = [];
     // 遍历数据
     for (var idx in movicesDouban.subjects) {
       var subject = movicesDouban.subjects[idx];
       var title = subject.title;
-      if(title.length>=6) {
-        title = title.substring(0,6)+"...";
+      if (title.length >= 6) {
+        title = title.substring(0, 6) + "...";
       }
       var temp = {
         stars: util.convertToStarsArray(subject.rating.stars),
-        title:title,
-        average:subject.rating.average,
-        coverageUrl:subject.images.large,
-        movieId:subject.id
+        title: title,
+        average: subject.rating.average,
+        coverageUrl: subject.images.large,
+        movieId: subject.id
       }
       movies.push(temp);
-      // 根据settedKey的不同，设置属性值。
-      var readyData = {};
-      readyData[settedKey]={
-        categoryTitle: categoryTitle,
-        movies:movies
-      };
-      this.setData(readyData);
     }
+    // 根据settedKey的不同，设置属性值。
+    var readyData = {};
+    readyData[settedKey] = {
+      categoryTitle: categoryTitle,
+      movies: movies
+    };
+    this.setData(readyData);
   },
-  onBindFocus:function(event) {
+  onBindFocus: function(event) {
     this.setData({
-      containerShow:false,
-      searchPannelShow:true
+      containerShow: false,
+      searchPannelShow: true
     });
-    
+
   },
-  onBindChange:function(event) {
-    console.log("show change");
+  onBindBlur: function(event) {
+    // 获取input组件中的值
+    var text = event.detail.value;
+    var searchUrl = app.globalData.doubanBase + "/v2/movie/search?q=" + text;
+    this.getMovieListData(searchUrl, "searchResult", "");
+
   },
-  onCancelImgTap:function(event) {
+  onCancelImgTap: function(event) {
     this.setData({
-      containerShow:true,
-      searchPannelShow:false
+      containerShow: true,
+      searchPannelShow: false,
+      searchResult: {}
     });
   }
 })
--- pages/posts/post-detail/post-detail.js
+++ pages/posts/post-detail/post-detail.js
@@ -6,7 +6,7 @@ Page({
    * 页面的初始数据
    */
   data: {
-    isPlayingMusic:false
+    isPlayingMusic: false
   },
 
   /**
@@ -34,24 +34,31 @@ Page({
       postsCollected[postId] = false;
       wx.setStorageSync("posts_collected", postsCollected);
     }
-    if (app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId===postId) {
-      this.setData({ 
-          isPlayingMusic:true
-          });
+    if (app.globalData.g_isPlayingMusic && app.globalData.g_currentMusicPostId === postId) {
+      this.setData({
+        isPlayingMusic: true
+      });
     }
     this.setMusicMonitor();
   },
-  setMusicMonitor:function() {
+  setMusicMonitor: function() {
     var that = this;
     // 监听音乐播放
-    wx.onBackgroundAudioPlay(function () {
+    wx.onBackgroundAudioPlay(function() {
       that.setData({
         isPlayingMusic: true
       });
       app.globalData.g_isPlayingMusic = true;
       app.globalData.g_currentMusicPostId = this.data.currentPostId;
     });
-    wx.onBackgroundAudioPause(function () {
+    wx.onBackgroundAudioPause(function() {
+      that.setData({
+        isPlayingMusic: false
+      });
+      app.globalData.g_isPlayingMusic = false;
+      app.globalData.g_currentMusicPostId = null;
+    });
+    wx.onBackgroundAudioStop(function() {
       that.setData({
         isPlayingMusic: false
       });
@@ -69,51 +76,55 @@ Page({
     this.showToast(postsCollected, postCollected);
   },
   // 模态框
-  showModal: function (postsCollected,postCollected) {
+  showModal: function(postsCollected, postCollected) {
     var that = this;
     wx.showModal({
       title: '收藏',
-      content: postCollected?"收藏该文章？":"取消收藏该文章？",
-      showCancel:true,
-      cancelText:'取消',
-      cancelColor:'#333',
-      confirmText:'确认',
-      confirmColor:'#405f80',
-      success:function(res) {
-        if(res.confirm) {
+      content: postCollected ? "收藏该文章？" : "取消收藏该文章？",
+      showCancel: true,
+      cancelText: '取消',
+      cancelColor: '#333',
+      confirmText: '确认',
+      confirmColor: '#405f80',
+      success: function(res) {
+        if (res.confirm) {
           // 更新当前文章的收藏状态缓存
           wx.setStorageSync("posts_collected", postsCollected);
           // 更新数据
-          that.setData({ collected: postCollected });
+          that.setData({
+            collected: postCollected
+          });
         }
       }
     })
   },
-  showToast: function (postsCollected, postCollected) {
+  showToast: function(postsCollected, postCollected) {
     var that = this;
     // 更新当前文章的收藏状态缓存
     wx.setStorageSync("posts_collected", postsCollected);
     // 更新数据
-    that.setData({ collected: postCollected });
+    that.setData({
+      collected: postCollected
+    });
     wx.showToast({
       title: postCollected ? "收藏成功" : "取消成功",
       duration: 1000,
       icon: "success"
     })
   },
-  onShareTap:function(event) {
-    var itemList=[
+  onShareTap: function(event) {
+    var itemList = [
       "分享给微信好友",
       "分享到朋友圈",
       "分享到QQ",
       "分享到微博"
     ]
     wx.showActionSheet({
-      itemList:itemList,
-      itemColor:"#405f80",
-      success:function(res) {
+      itemList: itemList,
+      itemColor: "#405f80",
+      success: function(res) {
         wx.showModal({
-          title: '用户'+itemList[res.tapIndex],
+          title: '用户' + itemList[res.tapIndex],
           content: '现在无法实现分享功能'
         });
       }
@@ -167,16 +178,16 @@ Page({
   onShareAppMessage: function() {
 
   },
-  onMusicTap:function(event) {
+  onMusicTap: function(event) {
     var isPlayingMusic = this.data.isPlayingMusic;
     var currentPostId = this.data.currentPostId;
     var postData = postsData.postList[currentPostId];
     if (isPlayingMusic) {
       wx.pauseBackgroundAudio();
       this.setData({
-        isPlayingMusic:false
+        isPlayingMusic: false
       });
-    }else {
+    } else {
       wx.playBackgroundAudio({
         dataUrl: postData.music.url,
         title: postData.music.title,
